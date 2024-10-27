f3968727318c8ea252ac4b3c4ff7b5e6baff0cfe
Alex <alexsanty10lm@gmail.com>
2024-10-27 12:43:58 -0500

    Se creacron metodos para obtener de distintas maneras a los usuarios en user.service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Clientes } from 'src/entity/Clientes';
 import { Empleados } from 'src/entity/Empleados';
@@ -12,4 +12,34 @@ export class UserService {
         @InjectRepository(Empleados)
         private readonly employeesRepository: Repository<Empleados>,
     ) { }
+
+    async getAllUsers() {
+        const clients = await this.getAllClients();
+        const employees = await this.getAllEmployees();
+        return { clients, employees };
+    }
+
+    async getClientById(id: number) {
+        const client = await this.clientsRepository.findOne({ where: { idCliente: id } });
+        if (!client) {
+            throw new NotFoundException(`Cliente con ID ${id} no encontrado`);
+        }
+        return client;
+    }
+
+    async getEmployeeById(id: number) {
+        const employee = await this.employeesRepository.findOne({ where: { idEmpleado: id } });
+        if (!employee) {
+            throw new NotFoundException(`Empleado con ID ${id} no encontrado`);
+        }
+        return employee;
+    }
+
+    async getAllClients() {
+        return this.clientsRepository.find();
+    }
+
+    async getAllEmployees() {
+        return this.employeesRepository.find();
+    }
 }
