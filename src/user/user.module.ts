0e040658f0939263673151e304fcd7b2d24037ac
Alex <alexsanty10lm@gmail.com>
2024-10-27 22:02:12 -0500

    Se cambiaron las rutas de la importación de archivos en user.module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Clientes } from 'src/entity/Clientes';
-import { Empleados } from 'src/entity/Empleados';
+import { Clientes } from 'src/entity/Clientes.entity';
+import { Empleados } from 'src/entity/Empleados.entity';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([Clientes, Empleados]),],
