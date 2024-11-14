43125d345b0ae6373a385b08dd1f9e7dc509b283
Alex <alexsanty10lm@gmail.com>
2024-11-14 03:07:22 -0500

    Se modifico auth.service para utilizar jwt

--- src/Auth/domain/auth.service.ts
+++ src/Auth/domain/auth.service.ts
@@ -1,32 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Users } from '../../entity/Users.entity';
-import * as bcrypt from 'bcrypt';
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/interface/user.service';
+import { Users } from 'src/entity/Users.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(Users)
-    private readonly usersRepository: Repository<Users>,
-  ) {}
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) { }
 
-  async validateUser(email: string, password: string): Promise<Users | null> {
-    const user = await this.usersRepository.findOne({
-      where: { email },
-      relations: ['role'],
-    });
-    if (user && (await bcrypt.compare(password, user.password))) {
-      return user;
-    }
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findUser(email);
+
+    if (user && (await compare(password, user.password))) return user;
     return null;
   }
 
-  isAdmin(user: Users): boolean {
-    return user.role.roleName === 'Administrador';
-  }
+  login(user: Users) {
+    const { userId, ...rest } = user;
+    const payload = { sub: userId };
 
-  isEmployee(user: Users): boolean {
-    return user.role.roleName === 'Empleado';
+    const response = {
+      user,
+      accessToken: this.jwtService.sign(payload),
+    };
+
+    return response;
   }
 }
