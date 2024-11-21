1bed420ff9cab77ed3806bc2119080ac520d5768
Alex <alexsanty10lm@gmail.com>
2024-11-20 21:52:13 -0500

    Se creó el metodo de la solicitud http para el restablecimiento de contraseña

--- src/Auth/application/auth.controller.ts
+++ src/Auth/application/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { Users } from 'src/entity/Users.entity';
 import { AuthService } from '../domain/auth.service';
 import { User } from 'src/common/decorators/user.decorator';
+import { RecoverPasswordService } from '../domain/recover-password.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService,
+    private readonly recoverPasswordService: RecoverPasswordService,
+  ) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
@@ -19,4 +22,24 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   profile() { }
+
+  @Post('recover-password')
+  @HttpCode(HttpStatus.OK)
+  async recoverPassword(@Body('email') email: string): Promise<{ message: string }> {
+    try {
+      await this.recoverPasswordService.recoverPasswordEmail(email);
+      return { message: 'Correo de recuperación enviado con éxito.' };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Post('reset-password')
+  async resetPassword(
+    @Body('token') token: string,
+    @Body('newPassword') newPassword: string,
+  ): Promise<{ message: string }> {
+    await this.recoverPasswordService.resetPassword(token, newPassword);
+    return { message: 'Contraseña restablecida correctamente' };
+  }
 }
