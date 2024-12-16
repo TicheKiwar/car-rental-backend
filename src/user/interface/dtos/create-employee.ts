4416e5507c11daf9b9050bad1276583524079e59
Alex <alexsanty10lm@gmail.com>
2024-12-15 23:54:20 -0500

    Se modificó el dto para crear empleados

--- src/user/interface/dtos/create-employee.ts
+++ src/user/interface/dtos/create-employee.ts
@@ -1,21 +1,23 @@
 import { IsDateString, IsDecimal, IsEmail, IsNumber, IsNumberString, IsOptional, IsString, Length } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
+import { IsEcuadorianId } from 'src/user/domain/user.decorator';
 
 export class CreateEmployeeDto extends CreateUserDto {
     @IsNumberString()
-    @Length(10)
+    @Length(10, 10, { message: 'El DNI debe tener exactamente 10 dígitos.' })
+    @IsEcuadorianId({ message: 'La cédula no es válida.' })
     dni: string;
 
-    @IsString()
-    @Length(1, 50)
+    @IsString({ message: 'El nombre debe ser una cadena de texto.' })
+    @Length(1, 50, { message: 'El nombre debe tener entre 1 y 50 caracteres.' })
     firstName: string;
 
-    @IsString()
-    @Length(1, 50)
+    @IsString({ message: 'El apellido debe ser una cadena de texto.' })
+    @Length(1, 50, { message: 'El apellido debe tener entre 1 y 50 caracteres.' })
     lastName: string;
 
     @IsNumberString()
-    @Length(10)
+    @Length(10, 10, { message: 'El teléfono debe tener exactamente 10 dígitos.' })
     phone: string;
 
     @IsNumber()
@@ -24,6 +26,6 @@ export class CreateEmployeeDto extends CreateUserDto {
     @IsDateString()
     hireDate: string;
 
-    @IsDecimal()
+    @IsNumber()
     salary: number;
 }
