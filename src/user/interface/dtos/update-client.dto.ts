d3f93f4f23d260ec879a44f89e53ef75f3128965
Alex <alexsanty10lm@gmail.com>
2024-12-15 23:54:51 -0500

    Se crearon dtos para la actualización de usuarios

---
+++ src/user/interface/dtos/update-client.dto.ts
@@ -0,0 +1,31 @@
+import { IsNumberString, IsOptional, IsString, Length } from 'class-validator';
+import { UpdateUserDto } from './update-user.dto';
+import { IsEcuadorianId } from 'src/user/domain/user.decorator';
+
+export class UpdateClientDto extends UpdateUserDto {
+    @IsOptional()
+    @IsNumberString()
+    @Length(10, 10, { message: 'El DNI debe tener exactamente 10 dígitos.' })
+    @IsEcuadorianId({ message: 'La cédula no es válida.' })
+    dni: string;
+
+    @IsOptional()
+    @IsString({ message: 'El nombre debe ser una cadena de texto.' })
+    @Length(1, 50, { message: 'El nombre debe tener entre 1 y 50 caracteres.' })
+    firstName: string;
+
+    @IsOptional()
+    @IsString({ message: 'El apellido debe ser una cadena de texto.' })
+    @Length(1, 50, { message: 'El apellido debe tener entre 1 y 50 caracteres.' })
+    lastName: string;
+
+    @IsOptional()
+    @IsNumberString()
+    @Length(10, 10, { message: 'El teléfono debe tener exactamente 10 dígitos.' })
+    phone: string;
+
+    @IsOptional()
+    @IsString({ message: 'La dirección debe ser una cadena de texto.' })
+    @Length(1, 100, { message: 'La dirección debe tener entre 1 y 100 caracteres.' })
+    address: string;
+}
